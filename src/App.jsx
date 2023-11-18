b89b00300079a75c7d8f8aeb0f999b8201ab3ca4
Anthony <anthony.rousseau26@gmail.com>
2023-11-18 17:38:01 +0100

    Final changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
+import Background from "./Components/Background/Background";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
 
 const App = () => {
+  let heroData = [
+    {text1:"Dive into", text2:"what you love"},
+    {text1:"Indulge", text2:"your passions"},
+    {text1:"Give in to", text2:"your passions"},
+  ]
+  const [heroCount, setHeroCount] = useState(0);
+  const [playStatus, setPlayStatus] = useState(false);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setHeroCount((prevCount) => prevCount === 2 ? 0 : prevCount + 1);
+    }, 5000);
+  }, []);
+
   return (
-    <div></div>
+    <div>
+      <Background playStatus={playStatus} heroCount={heroCount}/>
+      <Navbar/>
+      <Hero
+        setPlayStatus = {setPlayStatus}
+        heroData = {heroData[heroCount]}
+        heroCount = {heroCount}
+        setHeroCount = {setHeroCount}
+        playStatus = {playStatus}
+      />
+    </div>
   )
 }
 
